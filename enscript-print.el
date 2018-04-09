09777e54745a0f2a482754ff56d4d5147f632c51
Darren Embry <dse@webonastick.com>
2018-04-08 20:03:25 -0400

    bug fixes and feature enhancements

--- enscript-print.el
+++ enscript-print.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2018  Darren Embry
 
-;; Author: Darren Embry <dse@rectangle>
+;; Author: Darren Embry <dse@webonastick.com>
 ;; Keywords: tools
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -20,7 +20,10 @@
 
 ;;; Commentary:
 
-;; Nothing necessary at the moment.  :-)
+;; Two interactively called functions, `enscript-print-buffer' and
+;; `enscript-print-region', which are fancy wrappers that pipe the
+;; appropriate contents to GNU Enscript.  There are plenty of
+;; customizable options in the `enscript-print' customize group.
 
 ;;; Code:
 
@@ -28,10 +31,14 @@
   "Printing with GNU Enscript"
   :group 'printing)
 
-(defcustom enscript-print-header-p t
+(defcustom enscript-print-executable "enscript"
+  "Name of GNU Enscript executable."
+  :group 'enscript-print
+  :type '(string))
+
+(defcustom enscript-print-header t
   "Print using the enscript header."
   :group 'enscript-print
-  :tag "Print The Header?"
   :type '(boolean))
 
 (defcustom enscript-print-font-name nil
@@ -40,10 +47,9 @@
 If value is nil, either \"Courier\" or whatever is specified in
 ~/.enscriptrc is used."
   :group 'enscript-print
-  :tag "Font Name"
   :type '(choice (const :tags "Use Enscript's default" nil)
-                 (string :tags "Use Courier" "Courier")
-                 (string :tags "Custom Font")))
+                 (const :tags "Use Courier" "Courier")
+                 (string :tags "Custom Font" "Courier")))
 
 (defcustom enscript-print-font-size nil
   "The PostScript font size (in units of 1/72 inch) to use when printing.
@@ -59,96 +65,198 @@ specifies the font's width and `enscript-print-font-height'
 specifies the font's height.  Otherwise, this specifies both the
 font's width and height."
   :group 'enscript-print
-  :tag "Font Size"
   :type '(choice (const :tags "Use Enscript's default" nil)
-                 (number :tags "10pt" 10)
-                 (number :tags "7pt" 7)
-                 (number :tags "Custom font size in points")))
+                 (const :tags "10pt" 10)
+                 (const :tags "7pt" 7)
+                 (number :tags "Custom font size in points" 10)))
 
 (defcustom enscript-print-font-height nil
   "The PostScript font height (in units of 1/72 inch) to use when printing.
 
 If the value is nil, maintain the font's original aspect ratio."
   :group 'enscript-print
-  :tag "Font Height"
   :type '(choice (const :tags "Maintain font's original aspect ratio" nil)
-                 (number :tags "Custom font height in points")))
+                 (number :tags "Custom font height in points" 10)))
 
-(defcustom enscript-print-landscape-p nil
+(defcustom enscript-print-header-font-name nil
+  "The PostScript font name to use when printing the header.
+
+If value is nil, either \"Courier\" or whatever is specified in
+~/.enscriptrc is used."
+  :group 'enscript-print
+  :type '(choice (const :tags "Use Enscript's default" nil)
+                 (const :tags "Use Courier" "Courier")
+                 (string :tags "Custom Font" "Courier")))
+
+(defcustom enscript-print-header-font-size nil
+  "The PostScript font size to use when printing the header.
+
+If value is nil, either 10 points or whatever is specified in
+~/.enscriptrc is used.
+
+If landscape multi-column printing is done, the default font size
+changes from 10 points to 7 points.
+
+If a value for `enscript-print-header-font-height' is specified, this
+specifies the font's width and `enscript-print-header-font-height'
+specifies the font's height.  Otherwise, this specifies both the
+font's width and height."
+  :group 'enscript-print
+  :type '(choice (const :tags "Use Enscript's default" nil)
+                 (const :tags "10pt" 10)
+                 (const :tags "7pt" 7)
+                 (number :tags "Custom font size in points" 10)))
+
+(defcustom enscript-print-header-font-height nil
+  "The PostScript font height to use when printing the header.
+
+If the value is nil, maintain the font's original aspect ratio."
+  :group 'enscript-print
+  :type '(choice (const :tags "Maintain font's original aspect ratio" nil)
+                 (number :tags "Custom font height in points" 10)))
+
+(defcustom enscript-print-landscape nil
   "Whether to print in landscape orientation."
   :group 'enscript-print
-  :tag "Landscape?"
   :type '(boolean))
 
 (defcustom enscript-print-columns 1
   "Number of columns to print."
   :group 'enscript-print
-  :tag "Number of Columns"
   :type '(integer))
 
 (defcustom enscript-print-highlight-bars nil
   "Use highlight bars."
   :group 'enscript-print
-  :tag "Highlight Bars?"
   :type '(choice (const :tags "Off" nil)
                  (const :tags "On" t)
-                 (integer :tags "Height of highlight bars in lines")))
+                 (integer :tags "Height of highlight bars in lines" 2)))
 
-(defcustom enscript-print-highlight-bar-gray-level 0.1
-  "Highlight bar gray level (0 to 1)."
+(defcustom enscript-print-highlight-bar-gray-level 0.9
+  "Highlight bar gray level (0 is black; 1 is white)."
   :group 'enscript-print
-  :tag "Highlight Bar Gray Level"
   :type '(number))
 
-(defun enscript-print-shell-concat (command-line &rest arguments)
+(defcustom enscript-print-exclude-emacs-local-variables nil
+  "Exclude Emacs \"Local Variables\" sections."
+  :group 'enscript-print
+  :type '(boolean))
+
+(defcustom enscript-print-number-of-copies nil
+  "Number of copies to print."
+  :group 'enscript-print
+  :type '(choice (const :tags "Default" nil)
+                 (integer :tags "Number of copies" 2)))
+
+(defcustom enscript-print-truncate-lines nil
+  "Cut lines that are too long for the page."
+  :group 'enscript-print
+  :type '(boolean))
+
+(defcustom enscript-print-printer-name nil
+  "Printer name to send documents to."
+  :group 'enscript-print
+  :type '(choice (const :tags "Default printer" nil)
+                 (string :tags "Printer name" "printer")))
+
+(defcustom enscript-print-line-numbers nil
+  "Print line numbers?"
+  :type '(boolean))
+
+;; Stolen from https://stackoverflow.com/questions/969067/name-of-this-function-in-built-in-emacs-lisp-library
+(defun enscript-print/flatten (LIST)
+  "Flattens LIST."
+  (cond
+   ((atom LIST) (list LIST))
+   ((null (cdr LIST)) (enscript-print/flatten (car LIST)))
+   (t (append (enscript-print/flatten (car LIST)) (enscript-print/flatten (cdr LIST))))))
+
+(defun enscript-print/shell-concat (command-line &rest arguments)
   "Take COMMAND-LINE; append ARGUMENTS; return new command line."
   (let ((command-line command-line))
-    (dolist (argument arguments command-line)
+    (dolist (argument (enscript-print/flatten arguments) command-line)
       (if argument
           (setq command-line (concat command-line " " (shell-quote-argument argument)))))))
 
+(defun enscript-print/point-before-local-variables-line ()
+  "Return location of last non-blank line before \"Local Variables:\" line.
+
+Return nil if \"Local Variables:\" line is not found."
+  ;; based on files-x.el
+  (save-excursion
+    (goto-char (point-max))
+    (search-backward "\n\^L" (max (- (point-max) 3000) (point-min)) 'noerror)
+    (when (let ((case-fold-search t))
+            (search-forward (concat "Local Variables" ":") nil t))
+      (forward-line 0) ;beginning of line, ignoring field boundaries
+      (when (search-backward-regexp "[^[:space:]\r\n]" nil 'noerror) ;point now on last non-whitespace char
+        (forward-line 1)                ;beginning of next line
+        (point)))))                     ;return it
+
+(defun enscript-print/point-max ()
+  "Return end of buffer for enscript printing purposes.
+
+If `enscript-print-exclude-emacs-local-variables' is non-nil,
+return either the location after the end of the line containing
+the last non-whitespace character before the \"Local Variables:\"
+line, or the value of `(point-max)'.
+
+If `enscript-print-exclude-emacs-local-variables' is nil,
+return `(point-max)'."
+  (if enscript-print-exclude-emacs-local-variables
+      (or (enscript-print/point-before-local-variables-line)
+          (point-max))
+    (point-max)))
+
+(defun enscript-print/font-string (font-name font-size font-height)
+  "Given FONT-NAME, FONT-SIZE, and FONT-HEIGHT, return a font spec.
+
+The font spec is used as the value of the `--font' and
+`--header-font' options."
+  (if (or font-name font-size)
+      (concat (or font-name "Courier")
+              (if font-size
+                  (concat (format "@%g" font-size)
+                          (if font-height
+                              (format "/%g" font-height) ""))
+                ""))))
+
 (defun enscript-print-command-line ()
   "Return the command line for enscript printing."
-  (let ((command-line "enscript"))
-    (if (not enscript-print-header-p)
-        (setq command-line (enscript-print-shell-concat
-                            command-line
-                            "--no-header")))
-    (if (or enscript-print-font-name enscript-print-font-size)
-        (setq command-line (enscript-print-shell-concat
-                            command-line
-                            (concat "--font="
-                                    (or enscript-print-font-name "Courier")
-                                    (if enscript-print-font-size
-                                        (concat "@" (number-to-string enscript-print-font-size)
-                                                (if enscript-print-font-height
-                                                    (concat "/" (number-to-string enscript-print-font-height)))))))))
-    (if enscript-print-landscape-p
-        (setq command-line (enscript-print-shell-concat
-                            command-line
-                            "--landscape")))
-    (if enscript-print-columns
-        (setq command-line (enscript-print-shell-concat
-                            command-line
-                            (concat "--columns=" (number-to-string enscript-print-columns)))))
-    (if enscript-print-highlight-bars
-        (if (numberp enscript-print-highlight-bars)
-            (setq command-line (enscript-print-shell-concat
-                                command-line
-                                (format "--highlight-bars=%d" enscript-print-highlight-bars)
-                                (format "--highlight-bar-gray=%f" enscript-print-highlight-bar-gray-level)))
-          (setq command-line (enscript-print-shell-concat
-                              command-line
-                              "--highlight-bars"
-                              (format "--highlight-bar-gray=%f" enscript-print-highlight-bar-gray-level)))))
-
-    command-line))
+  (enscript-print/shell-concat
+   enscript-print-executable
+   (if (not enscript-print-header) "--no-header")
+   (if (or enscript-print-font-name enscript-print-font-size)
+       (format "--font=%s" (enscript-print/font-string
+                            enscript-print-font-name
+                            enscript-print-font-size
+                            enscript-print-font-height)))
+   (if (or enscript-print-header-font-name enscript-print-header-font-size)
+       (format "--header-font=%s" (enscript-print/font-string
+                                   enscript-print-header-font-name
+                                   enscript-print-header-font-size
+                                   enscript-print-header-font-height)))
+   (if enscript-print-landscape "--landscape")
+   (if enscript-print-columns
+       (format "--columns=%d" enscript-print-columns))
+   (if enscript-print-highlight-bars
+       (if (numberp enscript-print-highlight-bars)
+           (list (format "--highlight-bars=%d" enscript-print-highlight-bars)
+                 (format "--highlight-bar-gray=%g" enscript-print-highlight-bar-gray-level))
+         (list "--highlight-bars"
+               (format "--highlight-bar-gray=%g" enscript-print-highlight-bar-gray-level))))
+   (if enscript-print-number-of-copies
+       (format "--copies=%d" enscript-print-number-of-copies))
+   (if enscript-print-truncate-lines "--truncate-lines")
+   (if enscript-print-printer-name
+       (format "--printer=%s" enscript-print-printer-name))
+   (if enscript-print-line-numbers "--line-numbers")))
 
 ;;;###autoload
 (defun enscript-print-buffer ()
   "Print the contents of the buffer using enscript."
   (interactive)
-  (shell-command-on-region (point-min) (point-max) (enscript-print-command-line)))
+  (shell-command-on-region (point-min) (enscript-print/point-max) (enscript-print-command-line)))
 
 ;;;###autoload
 (defun enscript-print-region ()
